3909ae62eca0f8f953f786f49ae59ddb0067c989
Geoff <skyward17@outlook.com>
2017-10-25 18:05:30 -0500

    added: pop.css added: test_code.js feat: Added Window and gameOver() modals to html         new file:   css/pop.css         modified:   index.html         modified:   js/popups.js         new file:   js/test_code.js

---
+++ js/test_code.js
@@ -0,0 +1,10 @@
+//Background Color Selector
+
+//HTML
+<section>
+    <h2>Pick A Color</h2>
+    <input type="color" id="colorPicker">
+</section>
+
+//JS
+let colorSelect = document.getElementById("colorPicker").value;
