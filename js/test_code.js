9e340250a27e6e46c187b5923746341687b4a53e
Geoff <skyward17@outlook.com>
2017-12-18 20:52:26 -0600

    refactor: Refactored coding for score panel in html feat: Added css coding for score panel classes

--- js/test_code.js
+++ js/test_code.js
@@ -1,12 +1,13 @@
 /*
 Bugs/Fixes/Ideas:
-User clicks don't add when clicking through
 'Close' on winnerWinner and gameOver don't work
 After last card is played needs to be a delay before gameOver modal popups
 gameOver Modal 'Play Again' needs to clear the Modal
 function userClicks
 clear userClicks
-Add another span in html 
+Add another span in html
+Add AddStar
+Span then section?
 
 */
 
